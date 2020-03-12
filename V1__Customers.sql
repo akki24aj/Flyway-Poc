2da7c1a1458403cafde0f82782d87ba24b0563e7
Akshay Jagdale <akki.nitt@gmail.com>
2020-03-12 23:48:39 +0530

    Adding V1 sql

---
+++ V1__Customers.sql
@@ -0,0 +1,17 @@
+USE DemoData
+GO
+
+-- drop the table if it already exists and you have permission to delete
+-- be careful with this, there is not an undo feature with this command
+DROP TABLE IF EXISTS Customers
+
+CREATE TABLE Customers
+(
+StudentId    int          NOT NULL,
+FirstName    varchar(20)  NOT NULL, 
+LastName     varchar(20)  NOT NULL,
+DateOfBirth  date         NOT NULL,
+Address      varchar(30)  NULL,
+PhoneNumber  nvarchar(10) NULL,
+DepartmentId int          NOT NULL
+)
