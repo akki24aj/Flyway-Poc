41e2d087134567cfb3c074a509ef76dece82fdc5
BansalVinod <30682659+BansalVinod@users.noreply.github.com>
2020-04-09 20:29:42 +1000

    Update V5__amp.sql

--- V5__amp.sql
+++ V5__amp.sql
@@ -5,7 +5,7 @@ GO
 -- be careful with this, there is not an undo feature with this command
 DROP TABLE IF EXISTS People
 
-CREATE TABLE AMP2
+CREATE TABLE AMP3
 (
 StudentId    int          NOT NULL,
 FirstName    varchar(20)  NOT NULL, 
